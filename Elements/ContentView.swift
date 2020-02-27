116e3ac94aa7d99fb21c42c4a916d664f57aa3b0
Ing Vue <vue.ing014@gmail.com>
2020-02-27 08:13:50 -0600

    Update ContentView.swift

--- Elements/ContentView.swift
+++ Elements/ContentView.swift
@@ -9,8 +9,324 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text = "Hello, SwiftUI!"
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack (alignment: .leading) {
+            Button(action: { }) {
+                Text("1")
+                .foregroundColor(Color.black)
+                .multilineTextAlignment(.center)
+                .frame(width: 30.0, height: 30.0)
+            }
+            .padding()
+            .frame(width: 30.0, height: 30.0)
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("3")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("4")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+            }
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("11")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("12")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+            }
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("19")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("20")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("21")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("22")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("23")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("24")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("25")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+            }
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("37")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("38")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("39")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("40")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("41")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("42")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("43")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+            }
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("55")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("56")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                // leave button as blank
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("72")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("73")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("74")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("75")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+            }
+            
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("87")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("88")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                // leave button as blank
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("104")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("105")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("106")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("107")
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 30.0, height: 30.0)
+                }
+                .padding()
+                .frame(width: 30.0, height: 30.0)
+            }
+        }
     }
 }
 
